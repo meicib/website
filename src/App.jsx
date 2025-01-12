c2fcd6d65ac51e57ce0dbd2aed492d65e4476460
meicib <ameicib@berkeley.edu>
2025-01-12 01:45:21 -0800

    Wide screen width

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
   }, [sectionIds]);
 
   return (
-    <div className="h-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-slate-300 selection:text-blue-900 flex justify-center">
+    <div className="h-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-slate-300 selection:text-blue-900 flex justify-center w-full">
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className="grid grid-cols-1 md:grid-cols-2 h-screen w-full max-w-screen-2xl">
         {/* Left Container (No scroll) */}
